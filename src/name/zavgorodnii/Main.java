5e597e7d773d0995c2b1211c027d8d3a75ba8390
Slava Zavhorodnii <zavgorodniy@gmail.com>
2020-01-30 09:15:53 +0200

    first commit. empty project

--- src/name/zavgorodnii/Main.java
+++ src/name/zavgorodnii/Main.java
@@ -3,5 +3,12 @@ package name.zavgorodnii;
 public class Main {
 
     public static void main(String[] args) {
+        String string  = "{{[]}}";
+        System.out.println(checkString(string));
     }
+
+    private static boolean checkString(String string) {
+        return false;
+    }
+
 }
